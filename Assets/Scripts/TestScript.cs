48c20880a242378efcde7a59a57a13503500624a
i-tatte <i.tatte.informatics@gmail.com>
2021-09-16 21:25:40 +0900

    test commit

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Ç…Ç·Å[");
     }
 
     // Update is called once per frame
