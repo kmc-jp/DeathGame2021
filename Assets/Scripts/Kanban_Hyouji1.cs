11c9228b72a4bc25a4ca324cb5c71e0e0a3330c3
mudboat <f1e2stwmknt@yahoo.co.jp>
2021-10-25 09:46:56 +0900

    titlet(途中)
    背景やシーン遷移がまだですが一旦

---
+++ Assets/Scripts/Kanban_Hyouji1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kanban_Hyouji1 : MonoBehaviour
+{
+    
+    void OnCollisionStay2D(Collision2D collison){
+        if(collison.gameObject.name == "Player(仮)"){
+        Debug.Log("当たっている");
+        }
+    
+    }
+}
