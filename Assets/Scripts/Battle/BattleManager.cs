c7a56b3037c34529a7b4c0938a4b227cf4752908
nos <nos@kmc.gr.jp>
2021-10-27 06:28:17 +0900

    ITurnAction使う

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -16,13 +16,13 @@ public class BattleManager : MonoBehaviour
     private List<GameObject> enemyList;
     private Enemy enemy;
 
-    private List<TurnAction> turnActions;
+    private List<ITurnAction> turnActions;
     
     // Start is called before the first frame update
     void Start()
     {
         player = new Player();
-        turnActions = new List<TurnAction>();
+        turnActions = new List<ITurnAction>();
     
         UpdatePlayerStatusView();
     }
@@ -35,13 +35,13 @@ public class BattleManager : MonoBehaviour
 
     public void AttackButton()
     {
-        turnActions.Add(new TurnAction(player, enemy));
+        turnActions.Add(new AttackAction(player, enemy));
     }
 
     public void Execute()
     {
         // agi降順にソート
-        turnActions.Sort((a, b) => a.actor.status.Agi - b.actor.status.Agi);
+        turnActions.Sort((a, b) => a.Actor.status.Agi - b.Actor.status.Agi);
         turnActions.ForEach((a)=>
         {
             a.Exec();
