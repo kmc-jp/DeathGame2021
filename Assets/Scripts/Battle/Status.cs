5f31ffb0670f44515bfe77adbe86319fda9444fe
nos <nos@kmc.gr.jp>
2021-10-28 09:30:19 +0900

    Revert "HPをReactiveにしてみる"
    This reverts commit 421a22d3687a50209a15f9f6ac18bce0e819c104.

--- Assets/Scripts/Battle/Status.cs
+++ Assets/Scripts/Battle/Status.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UniRx;
 
 public class Status
 {
     public int MaxHp;
     public int MaxMp;
-    public ReactiveProperty<int> HpReactive;
-    public int Hp
-    {
-        get { return this.HpReactive.Value; }
-        set { this.HpReactive.Value = value; }
-    }
+    public int Hp;
     public int Mp;
     public int Atk;
     public int Def;
@@ -22,7 +16,6 @@ public class Status
     {
         this.MaxHp = hp;
         this.MaxMp = mp;
-        this.HpReactive = new ReactiveProperty<int>(hp);
         this.Hp = hp;
         this.Mp = mp;
         this.Atk = atk;
