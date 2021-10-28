ff32b58f123ba97d64627f24e147a3111e9b1dd4
nos <nos@kmc.gr.jp>
2021-10-28 19:50:01 +0900

    CoverSkill実装

---
+++ Assets/Scripts/Battle/Actions/CoverSkillAction.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverSkillAction : ISkillAction
+{
+    public SkillMaster Id = SkillMaster.Cover;
+    public string Name 
+    { 
+        get
+        {   
+            return SkillService.Instance.SkillNameMaster[Id];
+        }
+    }
+    public int MpCost { get; } = 0;
+    public int Priority { get; set; } = 1;
+
+    public Actor Actor { get; set; }
+
+    public Actor Target { get; set; }
+
+    private bool isBreak;
+
+    public CoverSkillAction() {  }
+    public CoverSkillAction(Actor _actor, Actor _target, bool _isBreak)
+    {
+        this.Actor = _actor;
+        this.Target = _target;
+        this.isBreak = _isBreak;
+    }
+
+    public bool Prepare()
+    {
+        return false;
+    }
+    
+    public bool Exec()
+    {
+        if (Actor.IsDead) return false;
+        if (isBreak)
+        {
+            Target.CoveredBy = null;
+            MessageWindow.Instance.MakeWindow($"{Actor.Name} は身代わりをやめた");
+            return true;
+        }
+        Target.CoveredBy = Actor;
+        MessageWindow.Instance.MakeWindow($"{Actor.Name} は身代わりになっている");
+        return true;
+    }
+}
--- Assets/Scripts/Battle/Actions/SkillService.cs
+++ Assets/Scripts/Battle/Actions/SkillService.cs
@@ -6,16 +6,19 @@ using System;
 public enum SkillMaster
 {
     Heal,
+    Cover,
 }
 
 public class SkillService : SingletonMonoBehaviour<SkillService>
 {
     public readonly Dictionary<SkillMaster, string> SkillNameMaster = new Dictionary<SkillMaster, string>(){
         { SkillMaster.Heal, "回復呪文"},
+        { SkillMaster.Cover, "身代わり"},
     };
 
     public readonly Dictionary<SkillMaster, Type> SkillTypeMaster = new Dictionary<SkillMaster, Type>(){
         { SkillMaster.Heal, typeof(HealSkillAction)},
+        { SkillMaster.Cover, typeof(CoverSkillAction)},
     };
     
     void Start()
@@ -30,6 +33,9 @@ public class SkillService : SingletonMonoBehaviour<SkillService>
         {
             case SkillMaster.Heal:
                 action = new HealSkillAction();
+                break;
+            case SkillMaster.Cover:
+                action = new CoverSkillAction();
                 break;
             default:
                 Debug.Log("No Such Skill");
