aea7a4d7010165fc5be22cc35f965ef4e8633aa4
inonoa <himura.kennsinn.degozaru@gmail.com>
2021-11-18 19:17:29 +0900

    Floor3の二体のルーチン(途中)

---
+++ Assets/Scripts/Battle/Enemys/Floor3EnemyB.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class Floor3EnemyB : RoutinedEnemy
+{
+    public Floor3EnemyB(EnemyBehaviour behaviour) : base(behaviour)
+    {
+        this.Name = "仮";
+        this.Status = new Status(2500, -1, -1, 0, 0); //HP以外仮
+    }
+
+    protected override IEnumerator<ITurnAction> GetRoutine()
+    {
+        while (true)
+        {
+            yield return Random.Range(0, 3) switch
+            {
+                0 => new AttackAction(this, EnemyActionUtil.SelectTarget(), new ConstDamage(170, 130)),
+                1 => new AttackAction(this, EnemyActionUtil.SelectTarget(), new ConstDamage(220, 180)),
+                2 => new LaughAction(this),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
