3fe3d9e11ff52f35fc455c940dec9a803777385d
inonoa <himura.kennsinn.degozaru@gmail.com>
2021-11-18 19:01:57 +0900

    Floor2Enemyのルーチン

--- Assets/Scripts/Battle/Enemys/Floor2Enemy.cs
+++ Assets/Scripts/Battle/Enemys/Floor2Enemy.cs
@@ -15,13 +15,7 @@ public class Floor2Enemy : RoutinedEnemy
     {
         while (true)
         {
-            yield return new CounterAction(this, 250);
-        }
-        
-        while (true)
-        {
-            int zero_one = Random.Range(0, 2);
-            if (zero_one == 0)
+            if (Random.Range(0, 2) == 0)
             {
                 Player target = EnemyActionUtil.SelectTarget();
 
@@ -37,8 +31,7 @@ public class Floor2Enemy : RoutinedEnemy
                 yield return new CounterAction(this, 250);
             }
             
-            zero_one = Random.Range(0, 2);
-            if (zero_one == 0)
+            if (Random.Range(0, 2) == 0)
             {
                 // 全体攻撃
                 yield return new AttackToAllAction(this, new ConstDamage(150, 150));
@@ -49,8 +42,18 @@ public class Floor2Enemy : RoutinedEnemy
                 Player target = EnemyActionUtil.SelectTarget();
                 yield return new AttacksInARowAction(this, target, new ConstDamage(90, 70), 4);
             }
-            
-            // 強攻撃
+
+            {
+                // 強攻撃
+                Player target = EnemyActionUtil.SelectTarget();
+
+                yield return new AttackAction
+                (
+                    this,
+                    target,
+                    new ConstDamage(330, 330)
+                );
+            }
         }
     }
 }
