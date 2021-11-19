e2c37575053421dc71573ce5c30cde9f8225a807
nos <nos@kmc.gr.jp>
2021-11-20 04:30:29 +0900

    他のとこでもPrefsUtil使う

--- Assets/Scripts/Door/DoorString.cs
+++ Assets/Scripts/Door/DoorString.cs
@@ -6,13 +6,13 @@ public class DoorStringClass : MonoBehaviour
 {
     public static readonly string SutefuriyaText0 = "";
     public static readonly string SutefuriyaText1 = "あなたの力を調整いたしましょうか";
-    public static readonly string SutefuriyaText2 = $"1:ステータスを変更する/2:{PlayerPrefs.GetString("PLAYER_NAME")}のわざを変更する";
-    public static readonly string SutefuriyaText3 = $"どなたのステータスを変更しますか？\n1:{PlayerPrefs.GetString("PLAYER_NAME")}　2: 相棒";
-    public static readonly string SutefuriyaText4 = $"{PlayerPrefs.GetString("PLAYER_NAME")}さんへの振り分けですね";
+    public static readonly string SutefuriyaText2 = $"1:ステータスを変更する/2:{PrefsUtil.GetPlayerName()}のわざを変更する";
+    public static readonly string SutefuriyaText3 = $"どなたのステータスを変更しますか？\n1:{PrefsUtil.GetPlayerName()}　2: 相棒";
+    public static readonly string SutefuriyaText4 = $"{PrefsUtil.GetPlayerName()}さんへの振り分けですね";
     public static readonly string SutefuriyaText5 = "";
     public static readonly string SutefuriyaText6 = "それではいってらっしゃいませ";
     public static readonly string SutefuriyaText7 = "";
-    public static readonly string SutefuriyaText8 = $"{PlayerPrefs.GetString("PLAYER_NAME")}さんのわざを変更します";
+    public static readonly string SutefuriyaText8 = $"{PrefsUtil.GetPlayerName()}さんのわざを変更します";
     public static readonly string SutefuriyaText9 = "";
     public static readonly string SutefuriyaText10 = "相棒さんの振り分けですね";
     public static readonly string SutefuriyaText11 = "";
