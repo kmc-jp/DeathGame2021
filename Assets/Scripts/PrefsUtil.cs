8c2411c2df1af6e85ecc7aed7f191fb5e9d168cf
nos <nos@kmc.gr.jp>
2021-11-19 13:52:06 +0900

    fix

--- Assets/Scripts/PrefsUtil.cs
+++ Assets/Scripts/PrefsUtil.cs
@@ -35,7 +35,7 @@ public static class PrefsUtil
 
     public static void SetPlayerStatus(int hp, int mp, int atk, int def, int agi )
     {
-         string status = JsonUtility.ToJson(new List<int>() {hp, mp, atk, def, agi});
+         string status = JsonUtility.ToJson(new AdditionalStatus(hp, mp, atk, def, agi));
          PlayerPrefs.SetString("PLAYER_STATUS", status);
     }
 
